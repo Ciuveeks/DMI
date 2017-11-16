951cda684466306a2ba6a26400a514b87fa497ef
Girts <girts.1997@gmail.com>
2017-11-16 16:04:42 +0200

    20171027_13_35

---
+++ PYTHON/sin_caur_summu_ver1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from math import sin
+
+X = input("Lietotaj, ludzu, ievadi argumentu (x:) ")
+print type(x)
+Y = sin(x)
+print "sin(%.2f)=%.2f"%(x,y)
+
+a0 = (-1)**0*x**1/(1)
+S0 = a0
+print "a0 = %6..2f S0 = %6.2f"%(a0,S0)
+
+a1 = (-1)**1*x**3/(1*2*3)
+S1 = a0+a1
+print "a1 = %6.2f S1 = %6.2f"%(a1,S1)
+
