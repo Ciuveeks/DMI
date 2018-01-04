49e14795636934b7203fc17d62865d2d62d2e8d2
ansiiis <girts.1997@gmail.com>
2018-01-04 18:44:54 +0200

    20172311_18_oo

---
+++ PYTHON/labdarbs2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from math import sinh
+import numpy as np
+
+
+print "   1000"
+print "   -----------"
+print "   \ "
+print "    \                        K"
+print "     \               (K+1)*X"
+print"s(x)=|    -----------------------------"
+print "     /                    K!"
+print "    / "
+print "   / "
+print "   -----------"
+print "   k=0"
+print
+print
+print
+
