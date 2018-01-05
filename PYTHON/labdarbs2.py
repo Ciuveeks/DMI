e35c1bad4a1e1e099e6d6d81617408cfc8d3538e
aijatikuma@inbox.lv <girts.1997@gmail.com>
2018-01-05 17:30:07 +0200

    20172311_18_oo

--- PYTHON/labdarbs2.py
+++ PYTHON/labdarbs2.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from math import sinh
+from math import exp
 import numpy as np
 
 
 print "   1000"
 print "   -----------"
-print "   \ "
-print "    \                        K"
-print "     \               (K+1)*X"
+print "  \\ "
+print "   \\                       K"
+print "    \\               (K+1)*X"
 print"s(x)=|    -----------------------------"
 print "     /                    K!"
 print "    / "
@@ -19,3 +20,42 @@ print
 print
 print
 
+
+                          
+print "                      k+1"
+print "      R=      x----------------"
+print "                       2"
+print "                      k"
+
+
+
+
+
+
+
+
+
+def mana_funkcija (x):
+    k = 0
+    a = 1
+    print ("Pirmais saskaitāmais: a%d = %6.2f")%(k,a)
+    S = a
+    while k<1000:
+        k = k + 1
+        R = x * (k+1)/(k**2)
+        a = a * R
+        S = S + a
+        if k>=999:
+            print (" a%d = %6.2f")%(k,a)
+    return S
+
+
+
+x =  1. *  input("Lietotāj, lūdzu ievadi argumentu (x):")
+#x = np.float128(input("Lietotāj, lūdzu ievadi argumentu (x):"))
+print type(x)
+y = (1+x)*exp(x)
+print "Standarta (1+%.2f)*exp(%.2f)=%.2f"%(x,x,y)
+
+yy = mana_funkcija(x)
+print "mana funkcija(%.2f)=%.2f"%(x,yy)
